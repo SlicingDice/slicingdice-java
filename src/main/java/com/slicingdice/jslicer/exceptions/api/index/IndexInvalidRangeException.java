993b99155f1cdafd0c331112ab64403df20e708d
João Eduardo Medeiros <joao@simbioseventures.com>
2017-03-24 19:23:20 -0300

    Make changes according to review

--- src/main/java/com/slicingdice/jslicer/exceptions/api/index/IndexInvalidRangeException.java
+++ src/main/java/com/slicingdice/jslicer/exceptions/api/index/IndexInvalidRangeException.java
@@ -3,7 +3,7 @@ package com.slicingdice.jslicer.exceptions.api.index;
 import com.slicingdice.jslicer.exceptions.api.SlicingDiceException;
 
 public class IndexInvalidRangeException extends SlicingDiceException {
-    public IndexInvalidRangeException(String message) {
+    public IndexInvalidRangeException(final String message) {
         super(message);
     }
 }
